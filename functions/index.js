48a2dedb2ddb5d8ca58da3dd347960264c82ac29
Shinji Wakui <wakui@altry.co.jp>
2025-10-21 08:19:24 +0900

    Allow Authorization header in CORS

--- functions/index.js
+++ functions/index.js
@@ -38,7 +38,7 @@ export default {
     const corsHeaders = {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
     };
 
     // ===== OPTIONS (CORSプリフライト)
